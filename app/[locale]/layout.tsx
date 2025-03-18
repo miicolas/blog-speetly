198cf707a85f9e4a9fe69645d2162e2224da0862
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-18 21:27:22 +0100

    fix: build action

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,34 +1,40 @@
+import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 import Header from "@/components/header";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default async function RootLayout({
-  children,
-  params: { locale },
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
-  let messages;
-  try {
-    messages = (await import(`@/messages/${locale}.json`)).default;
-  } catch (error) {
-    console.error("Error fetching messages:", error);
-    notFound();
-  }
+interface RootLayoutProps {
+    children: ReactNode;
+    params: Promise<{ locale: string }>;
+}
 
-  return (
-    <html lang={locale} className={inter.className}>
-      <body className="min-h-screen bg-background antialiased">
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <Header />
-          <main className="relative flex min-h-screen flex-col">
-            {children}
-          </main>
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+export default async function RootLayout({
+    children,
+    params,
+}: RootLayoutProps) {
+    const { locale } = await params;
+    let messages;
+    try {
+        messages = (await import(`@/messages/${locale}.json`)).default;
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+        notFound();
+    }
+
+    return (
+        <html lang={locale} className={inter.className}>
+            <body className="min-h-screen bg-background antialiased">
+                <NextIntlClientProvider locale={locale} messages={messages}>
+                    <Header />
+                    <main className="relative flex min-h-screen flex-col">
+                        {children}
+                    </main>
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
